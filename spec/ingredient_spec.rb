900b3a3c85c09803f2133d574e228a0f09c66c6e
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-08-02 09:18:33 -0600

    feat: add tests for iteration 1: pantry

---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,29 @@
+require './lib/ingredient'
+require './lib/pantry'
+require 'rspec'
+
+Rspec.describe Ingredient do
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  context 'Iteration 1' do
+    it 'ingredients exist' do
+      expect(@ingredient1).to be_an_instance_of(Ingredient)
+      expect(@ingredient2).to be_an_instance_of(Ingredient)
+    end
+
+    it 'ingredient 1 has a name' do
+      expect(@ingredient1.name).to eq("Cheese")
+    end
+
+    it 'ingredient 1 has a unit of measurement' do
+      expect(@ingredient1.unit).to eq("oz")
+    end
+
+    it 'ingredient 1 has an amount of calories' do
+      expect(@ingredient1.calories).to eq(50)
+    end
+  end
+end
---
+++ spec/pantry_spec.rb
@@ -0,0 +1,31 @@
+require './lib/ingredient'
+require './lib/pantry'
+require 'rspec'
+
+Rspec.describe Pantry do
+  before :each do
+    @pantry = Pantry.new
+  end
+
+  context 'Iteration 1' do
+    it 'pantry exists' do
+      expect(@pantry).to be_an_instance_of(Pantry)
+    end
+
+    it 'pantry is not stocked by default' do
+      expect(@pantry.stock).to eq({})
+    end
+
+    it 'pantry can stock check ingredients' do
+      expect(@pantry.stock_check(@ingredient1)).to eq(0)
+    end
+
+    it 'pantry can restock ingredients' do
+      @pantry.restock(@ingredient1, 5)
+      @pantry.restock(@ingredient1, 10)
+      expect(@pantry.stock_check(@ingredient1)).to eq(15)
+      @pantry.restock(@ingredient2, 7)
+      expect(@pantry.stock_check(@ingredient2)).to eq(7)
+    end
+  end
+end
